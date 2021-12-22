74f72559602c10aa40399a4f7c453e352283edce
stage2 <stage@brome.ma>
2021-12-22 18:11:27 +0100

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
+
 class _MyAppState extends State<MyApp> {
   Completer<GoogleMapController> _controller = Completer();
 
@@ -23,7 +25,30 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+
+  LatLng _lastMapPosition = _center;
+
+  void _onCameraMove(CameraPosition position){
+    _lastMapPosition = position.target;
+  }
   MapType _currentMapType = MapType.normal;
+
+  final Set<Marker> _markers = {};
+
+  void _onAddMarkerButtonPressed(){
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'really cool place',
+          snippet: '5 start rating',
+        ),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
+      )
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,22 +67,35 @@ class _MyAppState extends State<MyApp> {
                 zoom: 11.0,
               ),
               mapType: _currentMapType,
+              markers: _markers,
+              onCameraMove: _onCameraMove,
             ),
             Padding(
                 padding: const EdgeInsets.all(16.0),
-              child: Align(
+              child:
+              Align(
                 alignment: Alignment.topRight,
-                child: FloatingActionButton(
-                  onPressed: () => _onMapTypeButtonPressed(),
-                  materialTapTargetSize: MaterialTapTargetSize.padded,
-                  backgroundColor: Colors.green,
-                  child: const Icon(Icons.map , size: 36.0),
+                child:Column(
+                    children: <Widget>[
+                      FloatingActionButton(
+                        onPressed: () => _onMapTypeButtonPressed(),
+                        materialTapTargetSize: MaterialTapTargetSize.padded,
+                        backgroundColor: Colors.green,
+                        child: const Icon(Icons.map , size: 36.0),
+                      ),
+                      SizedBox(height: 40.0),
+                      FloatingActionButton(
+                        onPressed: _onAddMarkerButtonPressed,
+                        materialTapTargetSize: MaterialTapTargetSize.padded,
+                        backgroundColor: Colors.green,
+                        child: const Icon(Icons.add_location , size: 36.0),
+                      )
+                    ],
                 ),
               ),
             )
           ],
         )
-
       ),
     );
   }
